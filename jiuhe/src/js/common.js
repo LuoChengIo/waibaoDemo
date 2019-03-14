166c1385b46fc9ed2bad26dcad3d2510b743f0cb
LuoChengIo <374857968@qq.com>
2019-03-14 23:46:11 +0800

    头部

--- jiuhe/src/js/common.js
+++ jiuhe/src/js/common.js
@@ -1,9 +1,13 @@
 var animationend = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
 $(function(){
-	$('#mobileNav').on('click',function(){
+	$('.c-nav .c-more').on('click',function(){ // 导航
 		$('.mobile-head').removeClass('slideOutRight dn').addClass('animated slideInRight');
-	}) 
-	$('.mobile-head .m-close').on('click',function(){
+	})
+	$('.c-nav .c-back').on('click',function(){ // 返回
+		history.back()
+	})
+	$('.c-nav .c-title').text(document.title)
+	$('.mobile-head .m-close').on('click',function(){ //导航关闭
 		$('.mobile-head').removeClass('slideInRight').addClass('slideOutRight')
 		setTimeout(function() {
 			$('.mobile-head').addClass('dn');
